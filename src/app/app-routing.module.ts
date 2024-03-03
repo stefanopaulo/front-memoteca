328b2877edad793c85b155c224a0558e730791fb
Stefano <stefanopaulo47@gmail.com>
2024-03-03 15:09:37 -0300

    Navegação com roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListarPensamentoComponent} from "./components/pensamentos/listar-pensamento/listar-pensamento.component";
+import {CriarPensamentoComponent} from "./components/pensamentos/criar-pensamento/criar-pensamento.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: "listar-pensamento",
+    component: ListarPensamentoComponent
+  },
+  {
+    path: "criar-pensamento",
+    component: CriarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
